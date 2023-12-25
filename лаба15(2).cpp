a9c8382693147afc01b514fa281f8f965da62954
K1CLuY <148985694+K1CLuY@users.noreply.github.com>
2023-12-25 17:34:29 +0200

    Add files via upload

---
+++ лаба15(2).cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    int num1, num2, num3;
+
+
+    cout << "Введіть три числа: ";
+    cin >> num1 >> num2 >> num3;
+
+
+    if (num1 <= num2 && num1 <= num3) {
+        cout << "Найменше число: " << num1 << endl;
+    }
+    else if (num2 <= num1 && num2 <= num3) {
+        cout << "Найменше число: " << num2 << endl;
+    }
+    else {
+        cout << "Найменше число: " << num3 << endl;
+    }
+
+    return 0;
+}
