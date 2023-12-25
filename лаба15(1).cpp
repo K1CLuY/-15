594ab7302d5d45fd224e93782164ed876a339050
K1CLuY <148985694+K1CLuY@users.noreply.github.com>
2023-12-25 17:33:53 +0200

    Add files via upload

---
+++ лаба15(1).cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    int a, b;
+    cout << "Введіть два числа: ";
+    cin >> a >> b;
+
+    if (a > b) {
+        cout << " число: " << a << endl;
+    }
+    else {
+        cout << " числа " << a << " та " << b << endl;
+    }
+
+    return 0;
+}
